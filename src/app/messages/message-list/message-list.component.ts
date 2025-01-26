cfd34253b591e55be9003bb5b758b4f13e258a83
Matthew Neff <mpneff19@gmail.com>
2025-01-25 21:20:52 -0700

    finished the project

--- src/app/messages/message-list/message-list.component.ts
+++ src/app/messages/message-list/message-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-list',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.component.css'
 })
 export class MessageListComponent {
+  messages : Message[] = [
+    {
+      id: '1',
+      subject: 'This is a subject',
+      msgText: 'This is a message',
+      sender: 'Matthew Neff'
+    },
+    {
+      id: '2',
+      subject: 'This is another subject',
+      msgText: 'This is another message',
+      sender: 'Matthew Neff'
+    }
+  ];
 
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
