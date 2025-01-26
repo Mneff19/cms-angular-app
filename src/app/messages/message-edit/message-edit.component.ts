d9285adb79b301f4f04b12f060a8035627a3a860
Matthew Neff <mpneff19@gmail.com>
2025-01-25 21:04:18 -0700

    added message edit logic

--- src/app/messages/message-edit/message-edit.component.ts
+++ src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'app-message-edit',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.component.css'
 })
 export class MessageEditComponent {
+  currentSender = 'Matthew Neff';
+  @Output() addMessageEvent: EventEmitter<Message> = new EventEmitter<Message>();
+  @ViewChild('subject') subjectInputRef: ElementRef;
+  @ViewChild('msgText') msgTextInputRef: ElementRef;
 
+  onSendMessage() {
+    const subject = this.subjectInputRef.nativeElement.value;
+    const msgText = this.msgTextInputRef.nativeElement.value;
+    const newMessage = new Message('1', subject, msgText, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectInputRef.nativeElement.value = '';
+    this.msgTextInputRef.nativeElement.value = '';
+  }
 }
